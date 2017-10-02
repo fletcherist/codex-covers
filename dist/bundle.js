8daef044a66aeac66a78d71c8b93d29439bd1b4e
fletcherist <romanovexports@gmail.com>
2017-10-02 22:53:50 +0300

    [feature]: Implement button component

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,12 +63,85 @@
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */
+/******/ ({
+
+/***/ 0:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
+var _button = __webpack_require__(39);
+
+var _button2 = _interopRequireDefault(_button);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var HeadlineButton = _button2.default.bind(null, {
+  id: 'headline-button',
+  label: 'Headline'
+});
+
+
+var MainTextButton = _button2.default.bind(null, {
+  id: 'main-text-button',
+  label: 'Main text'
+});
+
+var AttachImageButton = _button2.default.bind(null, {
+  id: 'attach-image-button',
+  label: 'Image'
+});
+
+var App = function App() {
+  return '\n    ' + HeadlineButton().component + '\n    ' + MainTextButton().component + '\n    ' + AttachImageButton().component + '\n';
+};
+
+document.body.innerHTML = App();
+
+var attachListener = function attachListener(type, elementId, func) {
+  return document.getElementById(elementId).addEventListener(type, func);
+};
+
+var attachClickListener = attachListener.bind(null, 'click');
+
+attachClickListener(HeadlineButton().id, function () {
+  return console.log('im func');
+});
+attachClickListener(MainTextButton().id, function () {
+  return console.log('im another func');
+});
+attachClickListener(AttachImageButton().id, function () {
+  return console.log('im third func');
+});
+
+/***/ }),
+
+/***/ 39:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+exports.default = Button;
+var ButtonStyles = "\n    color: blue;\n    font-size: 34px;\n";
+
+function Button(props) {
+  var label = props.label,
+      id = props.id;
+
+  var componentId = Math.random().toString().slice(2);
+  return _extends({}, props, {
+    component: "\n      <button style=\"" + ButtonStyles + "\"\n        id=\"" + (id || componentId) + "\">\n        " + label + "\n      </button>\n    "
+  });
+}
+
 /***/ })
-/******/ ]);
+
+/******/ });
--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 // @flow
+import Button from './components/button'
 
+const HeadlineButton = Button.bind(null, {
+  id: 'headline-button',
+  label: 'Headline'
+})
+
+const MainTextButton = Button.bind(null, {
+  id: 'main-text-button',
+  label: 'Main text'
+})
+
+const AttachImageButton = Button.bind(null, {
+  id: 'attach-image-button',
+  label: 'Image'
+})
+
+const App = () => `
+    ${HeadlineButton().component}
+    ${MainTextButton().component}
+    ${AttachImageButton().component}
+`
+
+document.body.innerHTML = App()
+
+const attachListener = (
+  type: string,
+  elementId: string,
+  func: Function
+) => document.getElementById(elementId).addEventListener(type, func)
+
+const attachClickListener = attachListener.bind(null, 'click')
+
+attachClickListener(HeadlineButton().id, () => console.log('im func'))
+attachClickListener(MainTextButton().id, () => console.log('im another func'))
+attachClickListener(AttachImageButton().id, () => console.log('im third func'))
